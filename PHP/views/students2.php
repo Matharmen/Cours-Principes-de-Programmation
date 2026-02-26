a72e3ab10696b11fbc2738294326c283d2b02376
Matharmen <157318387+Matharmen@users.noreply.github.com>
2026-02-26 20:52:07 +0100

    Create students2.php

---
+++ PHP/views/students2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Liste des étudiants</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<h1>Liste des étudiants</h1>
+
+<ul>
+<?php foreach ($students as $student): ?>
+    <li>
+        <?= $student['name'] ?> (<?= $student['age']?> ans)
+    </li>
+<?php endforeach; ?>
+</ul>
