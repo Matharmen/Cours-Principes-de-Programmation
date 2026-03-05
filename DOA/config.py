85d9bb73152b1f74cb2e91550fb451ed68ecefd2
Matharmen <157318387+Matharmen@users.noreply.github.com>
2026-03-05 10:29:38 +0100

    Create config.py

---
+++ DOA/config.py
@@ -0,0 +1,11 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "root",
+    "password": "MySQLroot",
+    "database": "school_api"
+}
+
+#Evite une connexion globale
+#Permet gestion par requête
+#Plus proche d'une architecture production
